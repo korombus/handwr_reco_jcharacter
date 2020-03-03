f5960806e9530ca6d73dacc3b8b2a0a435c3466c
Takashi Hisaeda <korombus@users.noreply.github.com>
2020-03-03 10:58:45 +0900

    プラットフォームの違いによるパスの違いを吸収するように修正

--- script/data_cleansing.py
+++ script/data_cleansing.py
@@ -4,28 +4,23 @@ import os.path
 import cv2
 import numpy as np
 import argparse
+import platform
 from tqdm import tqdm
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--directory", default=os.getcwd()+r'\character_image\hiragana73')
-    parser.add_argument("-o", "--output", default=os.getcwd()+r'\character_image\conv_hiragana73')
-    args = parser.parse_args()
-
+def data_cleansing(args, dir_ref_char):
     # イメージクレンジングを行う画像のフォルダ一覧を取得
     img_dir_list = os.listdir(args.directory)
     for image_dir in img_dir_list:
 
         # クレンジングした画像の出力先フォルダが存在しない場合は作成
-        if not os.path.isdir(args.output+'\\'+image_dir):
-            os.mkdir(args.output+'\\'+image_dir)
+        if not os.path.isdir(args.output+dir_ref_char+image_dir):
+            os.makedirs(args.output+dir_ref_char+image_dir)
 
         # イメージの一覧を取得
-        img_list = os.listdir(args.directory+'\\'+image_dir)
+        img_list = os.listdir(args.directory+dir_ref_char+image_dir)
         for image in tqdm(img_list):
             # 画像をグレイスケール化
-            img = cv2.imread(args.directory+'\\'+image_dir+'\\'+image)
+            img = cv2.imread(args.directory+dir_ref_char+image_dir+dir_ref_char+image)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
             # ガウシアンフィルタでぼかしを掛けて、二値化を行う
@@ -38,4 +33,20 @@ if __name__ == "__main__":
             img = cv2.erode(img, kernel, iterations = 1)
 
             # クレンジングした画像を保存
-            cv2.imwrite(args.output+'\\'+image_dir+'\\'+image, img)
+            cv2.imwrite(args.output+dir_ref_char+image_dir+dir_ref_char+image, img) 
+
+
+if __name__ == "__main__":
+
+    pf = platform.system()
+    dir_ref_char = '/'
+
+    if pf == 'Windows':
+        dir_ref_char = '\\'
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", default=os.getcwd()+dir_ref_char+'character_image'+dir_ref_char+'hiragana73')
+    parser.add_argument("-o", "--output", default=os.getcwd()+dir_ref_char+'character_image'+dir_ref_char+'conv_hiragana73')
+    args = parser.parse_args()
+
+    data_cleansing(args, dir_ref_char)
